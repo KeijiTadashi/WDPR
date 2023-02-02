253c44183c6c5ea9c649cd2a0e44222ee5ce377c
dotDOTavi <105508779+dotDOTavi@users.noreply.github.com>
2023-02-02 15:39:17 +0100

    added cypress changed app names in workflow

--- src/frontend/src/helper/ApiPath.js
+++ src/frontend/src/helper/ApiPath.js
@@ -1,2 +1,2 @@
 export const apiPath = "http://localhost:5093/";
-// export const apiPath = "https://wdprwebapi.azurewebsites.net/";
+ // export const apiPath = "https://wdprapi.azurewebsites.net/";
